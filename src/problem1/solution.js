5daedf3b79bd10a7698fcae4e957f68f032bffc7
Chin Kiat Tan <tanch@LAPTOP-O8P678VI.localdomain>
2023-03-21 16:48:12 -0500

    completed problem 1

---
+++ src/problem1/solution.js
@@ -0,0 +1,24 @@
+var sum_to_n_a = function (n) {
+  var res = 0;
+  for (let i = 1; i <= n; i++) {
+    res += i;
+  }
+  return res;
+};
+
+var sum_to_n_b = function (n) {
+  if (n <= 0) {
+    return 0;
+  }
+  return (n * (n + 1)) / 2;
+};
+
+var sum_to_n_c = function (n) {
+  if (n <= 0) {
+    return 0;
+  } else {
+    return n + sum_to_n_c(n - 1);
+  }
+};
+
+console.log(sum_to_n_a(0));
